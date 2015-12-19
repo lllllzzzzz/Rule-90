31382d3052cb61f399b2ea9c1cfb8c097191050a
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2015-12-19 01:47:55 -0800

    Added list comprehensions and new functions

--- sierpinski.py
+++ sierpinski.py
@@ -1,48 +1,42 @@
 #!/usr/bin/python
  
 import sys
- 
-def rule90(config, generations):
+
+def step(gen):
+    """Return the next step of Rule 90 given a cell configuration."""
+
+    return [gen[i - 1] ^ gen[i + 1] for i in xrange(1, len(gen) - 1)]
+
+def rule90(gen, steps):
     """Print N generations of Rule 90 with initial cell configuration."""
    
-    for generation in xrange(0, generations):
-        printGeneration(config)
- 
-        next_config = ""
-        cells = [int(i) for i in config]
- 
-        for i in xrange(0, len(cells)):
-            if len(config) == 1:
-                next_config += "0"
-            elif i == 0:
-                next_config += str(0 ^ int(cells[i + 1]))
-            elif i == len(cells) - 1:
-                next_config += str(int(cells[i - 1]) ^ 0)
-            else:
-                next_config += str(cells[i - 1] ^ cells[i + 1])
- 
-        config = next_config
- 
-def printGeneration(config):
+    for i in xrange(0, steps):
+        print_gen(gen[1:-1])
+        gen = [0] + step(gen) + [0]
+
+def print_gen(gen):
     """Print generation of automaton given a configuration."""
- 
-    config = str.replace(config, "0", " ")
-    config = str.replace(config, "1", "*")
-    print config
- 
-def sierpinski(width, generations):
-    """Print N generations of the Sierpinski Triangle."""
- 
-    zeroes = "".join(["0" for i in xrange(0, width / 2)])
-    initial_config = zeroes + "1" + zeroes
-    rule90(initial_config, generations)
+
+    print "".join(["*" if i == 1 else " " for i in gen])
+
+def init_gen(width):
+    """Return initial configuration of Sierpinski Triangle given a width."""
+
+    return [int(i) for i in ("0" * (width / 2)) + "1" + ("0" * (width / 2))]
+
+def sierpinski(width, steps):
+    """Print N steps of the Sierpinski Triangle."""
+
+    rule90(init_gen(width), steps)
  
 def main():
     if len(sys.argv) != 3 or not (sys.argv[1] + sys.argv[2]).isdigit():
-        print "Usage:", sys.argv[0], "[width] [generations]"
+        print "Usage:", sys.argv[0], "[width] [steps]"
         sys.exit(0)
  
-    sierpinski(int(sys.argv[1]), int(sys.argv[2]))
+    width = int(sys.argv[1])
+    steps = int(sys.argv[2])
+    sierpinski(width, steps)
  
 if __name__ == "__main__":
     main()
